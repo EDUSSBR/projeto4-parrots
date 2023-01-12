86277536aabd5efecff6de17946580fd48e65599
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-01-12 10:47:28 -0300

    added turnCard logic and generation of shuffled cards elements

--- script.js
+++ script.js
@@ -1,48 +1,75 @@
-let imgSRC = [ ]
 let view = {
+    imgSRC: ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'],
     askForNumberOfCards: function askForNumberOfCards() {
         return window.prompt("Com quantas cartas deseja jogar? (ex: 4, 6, 8, 10, 12 ou 14)");
     },
     addNewCard: function addNewCard(id) {
-
         document.querySelector('main').innerHTML += `
-        <div id=${id} onclick="turnCard(this);" class="card">
+        <div id=${id}>   
+        <div onclick="turnCard(this);" class="card">
             <img src="./imgs/back.png" alt="card">
-        </div> `
+        </div> 
+        <div onclick="turnCard(this);" class="card hidden">
+            <img src="./imgs/${this.imgSRC[id]}.gif" alt="card">
+        </div> 
+        </div>   
+        
+        `
+    },
+    getAllCards: function getAllCards() {
+        this.cards = document.querySelectorAll('.card')
+        return this
+    },
+    setNewCards: function setNewCards(cardsToChange) {
+        let container = document.querySelector('#container')
+        container.setInnerHtml("")
+        container += this.addNewCard()
     }
 }
-console.log([1, 2, 3, 4, 5, 6].sort(comparador))
-function comparador() {
-    return Math.random() - 0.5;
-}
+
+
 let model = {
     setupGame: function setupGame() {
         let isNumberOfCardsOk = false
         this.numberOfCards;
+        this.cardsID = []
         do {
             this.numberOfCards = this.askForNumberOfCards()
             isNumberOfCardsOk = this.checkNumber(this.numberOfCards)
         }
         while (!isNumberOfCardsOk)
-        for (let i=0; i<this.numberOfCards; i++) {
-            this.addNewCard(i)
-                   
-        }
-
+        this.generateSetOfCards()
+        this.generateSetOfCards()
+        // document.querySelectorAll(".card").forEach(item => item.classList.remove('hidden'))
     },
     checkNumber: function checkNumber(num) {
         return (String(num).match(/[02468]$/) && num > 3 && num < 15) ? true : false
-    }
+    },
+
+    generateSetOfCards: function generateSetOfCards() {
+        for (let i = 0; i < (this.numberOfCards / 2); i++) {
+            this.cardsID.push(i)
+        }
+        this.cardsID.sort(() => (Math.random() - 0.5))
+        for (let i = 0; i < (this.numberOfCards / 2); i++) {
+            let cardID = this.cardsID.pop()
+            this.addNewCard(cardID)
+        }
+    },
+
 
 }
 let controller = Object.assign(Object.create(view), model)
 controller.setupGame()
 
 
-function turnCard(card){
-    
+function turnCard(card) {
+    card.parentNode
+   card.parentNode.querySelectorAll('div').forEach(el => {
+        el.classList.toggle('hidden')
+    });
 
-    console.log(card)
+    // console.log(card)
 
 
 }
